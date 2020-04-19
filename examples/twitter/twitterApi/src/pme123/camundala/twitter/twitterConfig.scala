a00fb52c6354b0d74650655b85b63fc59d30d569
mpa <pascal.mengelt@finnova.com>
2020-04-19 14:51:13 +0200

    moved twitter-auth.conf to twitter example.

--- examples/twitter/twitterApi/src/pme123/camundala/twitter/twitterConfig.scala
+++ examples/twitter/twitterApi/src/pme123/camundala/twitter/twitterConfig.scala
@@ -6,6 +6,7 @@ import zio._
 import zio.config.ConfigDescriptor._
 import zio.config.typesafe.TypesafeConfig
 import zio.config.{Config, ConfigDescriptor, config}
+import zio.console.Console
 
 object twitterConfig {
   type TwitterConfig = Has[Service]
@@ -24,7 +25,7 @@ object twitterConfig {
   /**
     * Reads the Twitter Authentication from the `twitter-auth.conf`.
     */
-  lazy val live: TaskLayer[TwitterConfig] = {
+  lazy val live: RLayer[Console, TwitterConfig] = {
 
     val tokenConfig =
       (string("key") |@|
@@ -36,11 +37,14 @@ object twitterConfig {
         nested("accessToken")(tokenConfig)
         ) (TwitterAuth.apply, TwitterAuth.unapply)
 
-    lazy val sourceLayer: TaskLayer[Config[TwitterAuth]] = TypesafeConfig.fromHoconFile(new File("twitter-auth.conf"), twitterAuthConfig)
+    val authFile = new File("./examples/twitter/twitter-auth.conf")
 
-    ZLayer.succeed(
+    lazy val sourceLayer: TaskLayer[Config[TwitterAuth]] = TypesafeConfig.fromHoconFile(authFile, twitterAuthConfig)
+
+    ZLayer.fromService(console =>
       new Service {
         def auth(): Task[TwitterAuth] =
+          console.putStrLn(    s"AuthConfig from: ${authFile.getAbsolutePath}") *>
           config[TwitterAuth]
             .provideLayer(sourceLayer)
       })
