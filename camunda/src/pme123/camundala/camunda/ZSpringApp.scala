29bc9c2612f05c5f74cdb80179f4f2caa1e38c17
mpa <pascal.mengelt@finnova.com>
2020-04-19 16:07:52 +0200

    created common SpringApp in camunda module

---
+++ camunda/src/pme123/camundala/camunda/ZSpringApp.scala
@@ -0,0 +1,29 @@
+package pme123.camundala.camunda
+
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import zio.{ZIO, ZManaged, console}
+import zio.console.Console
+
+
+trait ZSpringApp  extends zio.App {
+
+  /**
+    * create SpringApplication as a ZManaged Resource.
+    */
+  protected def managedSpringApp(clazz: Class[_], args: List[String]): ZManaged[Console, Throwable, ConfigurableApplicationContext] =
+    ZManaged.make(
+      console.putStrLn("Starting Spring Container...") *>
+        ZIO.effect(
+          SpringApplication.run(clazz, args: _*)
+        )
+    )(ctx =>
+      console.putStrLn("Spring Container Stopping...") *>
+        ZIO.effect(
+          if (ctx.isActive)
+            SpringApplication.exit(ctx)
+        ).catchAll(ex =>
+          console.putStrLn(s"Problem shutting down the Spring Container.\n${ex.getMessage}")
+        )
+    )
+}
--- examples/rest/src/pme123/camundala/examples/rest/RestApp.scala
+++ examples/rest/src/pme123/camundala/examples/rest/RestApp.scala
@@ -1,43 +1,22 @@
 package pme123.camundala.examples.twitter
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.ConfigurableApplicationContext
+import pme123.camundala.camunda.ZSpringApp
+import zio.ZIO
 import zio.console.Console
-import zio.{ZIO, ZManaged, console}
 
 @SpringBootApplication
 @EnableProcessApplication
 class RestApp
 
-object RestApp extends zio.App {
+object RestApp extends ZSpringApp {
 
   def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
-    managedSpringApp(args).useForever
+    managedSpringApp(classOf[RestApp], args).useForever
       .provideLayer(Console.live)
       .fold(
         _ => 1,
         _ => 0
       )
-
-  /**
-    * create SpringApplication as a ZManaged Resource.
-    */
-  private def managedSpringApp(args: List[String]): ZManaged[Console, Throwable, ConfigurableApplicationContext] =
-    ZManaged.make(
-      console.putStrLn("Starting Spring Container...") *>
-        ZIO.effect(
-          SpringApplication.run(classOf[RestApp], args: _*)
-        )
-    )(ctx =>
-      console.putStrLn("Spring Container Stopping...") *>
-        ZIO.effect(
-          if (ctx.isActive)
-            SpringApplication.exit(ctx)
-        ).catchAll(ex =>
-          console.putStrLn(s"Problem shutting down the Spring Container.\n${ex.getMessage}")
-        )
-    )
-
 }
