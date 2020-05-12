2ff3db00f6ed74c1192736f15c4bcb7f06cab53b
Pascal Mengelt <pascal.mengelt@gmail.com>
2020-05-12 20:44:17 +0200

    moved TwitterApp to StandardApp

--- examples/twitter/src/pme123/camundala/examples/twitter/TwitterApp.scala
+++ examples/twitter/src/pme123/camundala/examples/twitter/TwitterApp.scala
@@ -1,31 +1,19 @@
 package pme123.camundala.examples.twitter
 
-import java.io.InputStreamReader
 import java.nio.file.{Path, Paths}
 
-import javax.script.ScriptEngineManager
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import pme123.camundala
-import pme123.camundala.app.appRunner
-import pme123.camundala.app.appRunner.AppRunner
 import pme123.camundala.camunda.CamundaLayers._
 import pme123.camundala.camunda.{CamundaLayers, ZSpringApp}
 import pme123.camundala.cli.cliApp.CliApp
 import pme123.camundala.cli.{ProjectInfo, cliApp}
 import pme123.camundala.config.ConfigLayers
 import pme123.camundala.model.ModelLayers
-import pme123.camundala.model.bpmn.bpmnRegister
-import pme123.camundala.model.bpmn.bpmnRegister.BpmnRegister
-import pme123.camundala.model.deploy.deployRegister.DeployRegister
-import pme123.camundala.model.deploy.{Deploys, deployRegister}
-import pme123.camundala.services.httpServer.HttpServer
-import pme123.camundala.services.{ServicesLayers, httpServer}
+import pme123.camundala.services.{ServicesLayers, StandardApp, httpServer}
 import zio._
 import zio.clock.Clock
 import zio.console.Console
-import zio.stm.TRef
-
-import scala.io.Source
 
 @SpringBootApplication
 class TwitterApp
@@ -51,60 +39,9 @@ object TwitterApp extends ZSpringApp {
         _ => 0
       )
 
-  type TwitterAppDeps = Console with DeployRegister with BpmnRegister with HttpServer
-  private lazy val twitterApp: ZLayer[TwitterAppDeps, Nothing, AppRunner] =
-    ZLayer.fromServicesM[Console.Service, bpmnRegister.Service, deployRegister.Service, httpServer.Service, Any, Nothing, appRunner.Service](
-      (console, bpmnRegService, deplRegService, httpServService) =>
-
-        ZIO.foreach(0 to 1)(_ => TRef.make[Option[Fiber.Runtime[Throwable, Unit]]](None).commit).map {
-          case camundaRef :: httpServerRef :: _ =>
-
-            new appRunner.Service {
-
-              def start(): Task[Unit] = for {
-                httpServerFiber <- httpServService.serve().fork
-                _ <- httpServerRef.set(Some(httpServerFiber)).commit
-                _ <- update()
-                camundaFork <- managedSpringApp(classOf[TwitterApp]).useForever.fork.provideLayer(ZLayer.succeed(console))
-                _ <- camundaRef.set(Some(camundaFork)).commit
-              } yield ()
-
-              def update(): Task[Unit] = for {
-                deploys <- readScript
-                _ <- ZIO.foreach(deploys.value.flatMap(_.bpmns))(b => bpmnRegService.registerBpmn(b))
-                _ <- ZIO.foreach(deploys.value)(d => deplRegService.registerDeploy(d))
-              } yield ()
-
-              def stop(): Task[Unit] = (for {
-                maybeHttpFiber <- httpServerRef.get.commit
-                httpFiber <- ZIO.fromOption(maybeHttpFiber).mapError(_ => new Exception("Service already down"))
-                _ <- httpFiber.interrupt
-                maybeCamundaFiber <- camundaRef.get.commit
-                camFiber <- ZIO.fromOption(maybeCamundaFiber).mapError(_ => new Exception("Service already down"))
-                _ <- camFiber.interrupt
-              } yield ())
-
-              def restart(): Task[Unit] =
-                stop() *> start()
-            }
-        }
-    )
+  private lazy val twitterApp = StandardApp.layer(classOf[TwitterApp], bpmnModelsPath)
 
   private val bpmnModelsPath: Path = Paths.get(".", "examples", "twitter", "resources", "bpmnModels.sc")
-  private lazy val readScript: Task[Deploys] = bpmnModels
-    .use { deploysReader =>
-      for {
-        e <- ZIO.effect(new ScriptEngineManager().getEngineByName("scala"))
-        scriptResult <- ZIO.effect(e.eval(deploysReader))
-        deploys <- scriptResult match {
-          case d: Deploys => UIO(d)
-          case other => Task.fail(new Exception(s"Script did not contain Deploys: $other"))
-        }
-      } yield deploys
-    }
-
-  private lazy val bpmnModels: Managed[Throwable, InputStreamReader] = ZManaged.make(ZIO.effect(Source.fromFile(bpmnModelsPath.toFile).reader()))(r =>
-    ZIO.effect(r.close()).catchAll(e => UIO(e.printStackTrace()) *> UIO.unit))
 
   private lazy val cliLayer = (Clock.live ++ Console.live ++ CamundaLayers.bpmnServiceLayer ++ ModelLayers.deployRegisterLayer ++ CamundaLayers.deploymentServiceLayer ++ ServicesLayers.dockerComposerLayer ++ twitterApp) >>> cliApp.live
   private lazy val httpServerLayer = ConfigLayers.appConfigLayer ++ deploymentServiceLayer ++ ModelLayers.logLayer("httpServer") >>> httpServer.live
---
+++ services/src/pme123/camundala/services/StandardApp.scala
@@ -0,0 +1,96 @@
+package pme123.camundala.services
+
+import java.io.InputStreamReader
+import java.nio.file.Path
+
+import javax.script.ScriptEngineManager
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import pme123.camundala.app.appRunner
+import pme123.camundala.app.appRunner.AppRunner
+import pme123.camundala.model.bpmn.bpmnRegister
+import pme123.camundala.model.bpmn.bpmnRegister.BpmnRegister
+import pme123.camundala.model.deploy.{Deploys, deployRegister}
+import pme123.camundala.model.deploy.deployRegister.DeployRegister
+import pme123.camundala.services.httpServer.HttpServer
+import zio.{Fiber, Managed, Task, UIO, ZIO, ZLayer, ZManaged, console}
+import zio.console.Console
+import zio.stm.TRef
+
+import scala.io.Source
+
+object StandardApp {
+
+  type StandardAppDeps = Console with DeployRegister with BpmnRegister with HttpServer
+
+  def layer(clazz: Class[_], bpmnModelsPath: Path): ZLayer[StandardAppDeps, Nothing, AppRunner] =
+    ZLayer.fromServicesM[Console.Service, bpmnRegister.Service, deployRegister.Service, httpServer.Service, Any, Nothing, appRunner.Service](
+      (console, bpmnRegService, deplRegService, httpServService) =>
+
+        ZIO.foreach(0 to 1)(_ => TRef.make[Option[Fiber.Runtime[Throwable, Unit]]](None).commit).map {
+          case camundaRef :: httpServerRef :: _ =>
+
+            new appRunner.Service {
+
+              def start(): Task[Unit] = for {
+                httpServerFiber <- httpServService.serve().fork
+                _ <- httpServerRef.set(Some(httpServerFiber)).commit
+                _ <- update()
+                camundaFork <- managedSpringApp(clazz).useForever.fork.provideLayer(ZLayer.succeed(console))
+                _ <- camundaRef.set(Some(camundaFork)).commit
+              } yield ()
+
+              def update(): Task[Unit] = for {
+                deploys <- readScript(bpmnModelsPath)
+                _ <- ZIO.foreach(deploys.value.flatMap(_.bpmns))(b => bpmnRegService.registerBpmn(b))
+                _ <- ZIO.foreach(deploys.value)(d => deplRegService.registerDeploy(d))
+              } yield ()
+
+              def stop(): Task[Unit] = (for {
+                maybeHttpFiber <- httpServerRef.get.commit
+                httpFiber <- ZIO.fromOption(maybeHttpFiber).mapError(_ => new Exception("Service already down"))
+                _ <- httpFiber.interrupt
+                maybeCamundaFiber <- camundaRef.get.commit
+                camFiber <- ZIO.fromOption(maybeCamundaFiber).mapError(_ => new Exception("Service already down"))
+                _ <- camFiber.interrupt
+              } yield ())
+
+              def restart(): Task[Unit] =
+                stop() *> start()
+            }
+        }
+    )
+  /**
+    * create SpringApplication as a ZManaged Resource.
+    */
+  def managedSpringApp(clazz: Class[_], args: List[String] = List.empty): ZManaged[Console, Throwable, ConfigurableApplicationContext] =
+    ZManaged.make(
+      console.putStrLn("Starting Spring Container...") *>
+        ZIO.effect(
+          SpringApplication.run(clazz, args: _*)
+        )
+    )(ctx =>
+      console.putStrLn("Spring Container Stopping...") *>
+        ZIO.effect(
+          if (ctx.isActive)
+            SpringApplication.exit(ctx)
+        ).catchAll(ex =>
+          console.putStrLn(s"Problem shutting down the Spring Container.\n${ex.getMessage}")
+        )
+    )
+
+  private def readScript(bpmnModelsPath: Path): Task[Deploys] = bpmnModels(bpmnModelsPath)
+    .use { deploysReader =>
+      for {
+        e <- ZIO.effect(new ScriptEngineManager().getEngineByName("scala"))
+        scriptResult <- ZIO.effect(e.eval(deploysReader))
+        deploys <- scriptResult match {
+          case d: Deploys => UIO(d)
+          case other => Task.fail(new Exception(s"Script did not contain Deploys: $other"))
+        }
+      } yield deploys
+    }
+
+  private def bpmnModels(bpmnModelsPath: Path): Managed[Throwable, InputStreamReader] = ZManaged.make(ZIO.effect(Source.fromFile(bpmnModelsPath.toFile).reader()))(r =>
+    ZIO.effect(r.close()).catchAll(e => UIO(e.printStackTrace()) *> UIO.unit))
+}
