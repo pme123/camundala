0d738c017180f68accffac6961ebdd8fc7f68d5f
Pascal Mengelt <pascal.mengelt@gmail.com>
2022-01-15 20:10:09 +0100

    Cleanup examples.

--- examples/twitter/src/main/scala/camundala/examples/twitter/bpmn/TwitterApiCreator.scala
+++ examples/twitter/src/main/scala/camundala/examples/twitter/bpmn/TwitterApiCreator.scala
@@ -8,7 +8,7 @@ import camundala.examples.twitter.bpmn.TwitterApi.*
 import io.circe.generic.auto.*
 import sttp.tapir.generic.auto.*
 
-object TwitterApiCreator extends APICreator {
+object TwitterApiCreator extends APICreator:
 
   def title = "Twitter Process API"
 
@@ -19,12 +19,10 @@ object TwitterApiCreator extends APICreator {
   override def basePath: Path = pwd / "examples" / "twitter"
 
   apiEndpoints(
-      twitterDemoProcess
-        .endpoints(
-          reviewTweetApprovedUT.endpoint
-            .withOutExample("Tweet accepted", ReviewTweet())
-            .withOutExample("Tweet rejected", ReviewTweet(false)),
-        )
-    )
-
-}
+    twitterDemoProcess
+      .endpoints(
+        reviewTweetApprovedUT.endpoint
+          .withOutExample("Tweet accepted", ReviewTweet())
+          .withOutExample("Tweet rejected", ReviewTweet(false))
+      )
+  )
