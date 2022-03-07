6bce523149d6f2bde448fada03b6af168fb77041
Pascal Mengelt <pascal.mengelt@gmail.com>
2022-03-07 20:50:43 +0100

    Fixed Validator.

--- camunda/src/main/scala/camundala/camunda/Validator.scala
+++ camunda/src/main/scala/camundala/camunda/Validator.scala
@@ -13,8 +13,8 @@ import org.camunda.spin.impl.json.jackson.JacksonJsonNode
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.*
 
-abstract class Validator[T <: Product: Encoder: Decoder]
-    extends ExecutionListener :
+abstract class Validator[T <: Product : Encoder : Decoder]
+  extends ExecutionListener :
 
   def product: T
 
@@ -23,27 +23,13 @@ abstract class Validator[T <: Product: Encoder: Decoder]
     val ir = product.productElementNames.toSeq
       .map { k =>
         val typedValue: TypedValue = execution.getVariableTyped(k)
-        val value = typedValue.getType match
-          case _: PrimitiveValueType =>
-            typedValue.getValue match
-              case vt: DmnValueType =>
-                vt.asJson
-              case other =>
-                println(s"UneXPECTED: $other")
-                other
-          case _: SerializableValueType => typedValue.getValue
-          case _: FileValueType =>
-            typedValue match
-              case f: FileValueImpl =>
-                FileInOut(
-                  f.getFilename,
-                  f.getByteArray.take(10), // just take a few
-                  Option(f.getMimeType)
-                ).asJson
-              case o =>
-                throwErr(s"Must be a FileValueImpl - but is ${o.getClass}")
-        s""""$k": $value"""
-      }
+        if (typedValue == null)
+          println(s"NOT SET: $k")
+          "NOT_SET"
+        else
+          val value = extractValue(typedValue)
+          s""""$k": $value"""
+      }.filterNot(_ == "NOT_SET")
       .mkString("{", ",", "}")
     println(s"VALIDATOR JSON: ${toJson(ir)}")
     toJson(ir).as[T] match
@@ -51,7 +37,26 @@ abstract class Validator[T <: Product: Encoder: Decoder]
         println(s"Validation Succeeded (${product.getClass.getSimpleName})")
       case Left(ex) =>
         println(s"Validation Failed $ex (${product.getClass.getSimpleName})")
-        throw new BpmnError("Validation Error", s"Input is not valid: $ex")
+        throw new IllegalArgumentException(s"Validation Error: Input is not valid: $ex")
 
 
-
+  private def extractValue(typedValue: TypedValue) =
+    typedValue.getType match
+      case _: PrimitiveValueType =>
+        typedValue.getValue match
+          case vt: DmnValueType =>
+            vt.asJson
+          case other =>
+            println(s"UneXPECTED: $other")
+            other
+      case _: SerializableValueType => typedValue.getValue
+      case _: FileValueType =>
+        typedValue match
+          case f: FileValueImpl =>
+            FileInOut(
+              f.getFilename,
+              f.getByteArray.take(10), // just take a few
+              Option(f.getMimeType)
+            ).asJson
+          case o =>
+            throwErr(s"Must be a FileValueImpl - but is ${o.getClass}")
