fdf0de222b2fcd67fa674c2fdab4cc5e3bd16c08
Pascal Mengelt <pascal.mengelt@gmail.com>
2023-04-12 07:17:42 +0200

    MAP-5895: Adjustment for Page navigation.

--- api/src/main/scala/camundala/api/CompanyDocCreator.scala
+++ api/src/main/scala/camundala/api/CompanyDocCreator.scala
@@ -1,25 +1,28 @@
 package camundala.api
 
-/**
- * the idea is use Camundala to create Company's Process documentation.
- *
- * For a Start you can create a Catalog.
- */
+/** the idea is use Camundala to create Company's Process documentation.
+  *
+  * For a Start you can create a Catalog.
+  */
 trait CompanyDocCreator:
   def apiConfig: ApiConfig
 
   def createCatalog(): Unit =
     val projectPaths = apiConfig.gitConfigs.init
-    val catalogs = s"""## Catalog
-                   |${projectPaths
+    val catalogs = s"""{%
+                      |// auto generated - do not change!
+                      |helium.site.pageNavigation.depth = 1
+                      |%}
+                      |## Catalog
+                      |${projectPaths
       .map { case projectName -> _ =>
         val path = apiConfig.gitConfigs.gitDir / projectName / catalogFileName
         if (os.exists(path))
           os.read(path)
         else
           s"""### $projectName
-             |Sorry there is no $path.
-             |""".stripMargin
+                      |Sorry there is no $path.
+                      |""".stripMargin
       }
       .mkString("\n")}""".stripMargin
     val catalogPath = apiConfig.basePath / "src" / "docs" / catalogFileName
