9d2dd10e5edc8b32e14e7a9b6d8d183fc257dba9
Pascal Mengelt <pascal.mengelt@gmail.com>
2024-04-07 20:45:56 +0200

    Adjusted ComposedWorker Example.

--- 05-examples/invoice/03-worker/src/main/scala/camundala/examples/invoice/worker/ComposedWorker.scala
+++ 05-examples/invoice/03-worker/src/main/scala/camundala/examples/invoice/worker/ComposedWorker.scala
@@ -1,9 +1,10 @@
 package camundala.examples.invoice.worker
 
+import camundala.bpmn.GeneralVariables
 import camundala.examples.invoice.bpmn.ComposedWorkerExample.*
 import camundala.examples.invoice.bpmn.{StarWarsPeople, StarWarsPeopleDetail}
 import camundala.worker.CamundalaWorkerError.CustomError
-import camundala.worker.CustomWorkerDsl
+import camundala.worker.{CustomWorkerDsl, EngineRunContext}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 
@@ -23,26 +24,27 @@ class ComposedWorker
       inputObject: In
   ): Either[CustomError, Out] =
     logger.info("Do some crazy things running work...")
+    given EngineRunContext =
+      EngineRunContext(engineContext, GeneralVariables())
+
     val peopleWorkerIn = StarWarsPeople.In()
-    val out = peopleWorker.runWork(peopleWorkerIn)
+    val out = peopleWorker.worker.runWorkHandler
+      .map: handler =>
+        handler.runWork(peopleWorkerIn)
+          .left.map: error =>
+            CustomError(
+              s"Error while fetching Starwars People:\n- ${error.errorMsg}."
+            )
+      .getOrElse(Right(StarWarsPeople.Out.Failure()))
     out
-      .flatMap:
+      .map:
         case StarWarsPeople.Out.Success(people, _) =>
-          import cats.implicits.*
-          people.zipWithIndex
-            .map:
-              case (_, index) =>
-                val detail = StarWarsPeopleDetail.In(index)
-                peopleDetailWorker.runWork(detail)
-            .sequence
-        case _ =>
-          Right(Seq.empty)
-      .map: r =>
-        Out(
-          r.collect:
-            case StarWarsPeopleDetail.Out.Success(detail, _, _) =>
-              detail
-        )
+          logger.info(s"- Got People: $people")
+          Out()
+        case StarWarsPeople.Out.Failure(processStatus) =>
+          logger.info(s"- Got People failed with: $processStatus")
+          Out()
+
 
   end runWork
 
