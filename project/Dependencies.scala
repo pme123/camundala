1c0f49d86c23e6d9b12748db5e0a3fee68ce0a9b
Scala Steward <scala_steward@virtuslab.com>
2024-06-08 16:53:12 +0000

    Update circe to 3.8.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 import sbt.Keys.*
 
-object Dependencies {
+object Dependencies:
 
   // dependency Versions
   // 00-documentation
@@ -23,7 +23,7 @@ object Dependencies {
   // - mUnitVersion
   // 03-dmn
   // -> bpmn
-  val sttpClient3Version = "3.8.13"
+  val sttpClient3Version = "3.8.16"
   val dmnTesterVersion = "0.17.9"
   // - mUnitVersion
   // 03-simulation
@@ -56,7 +56,6 @@ object Dependencies {
   val twitter4jVersion = "4.1.2"
   val groovyVersion = "3.0.16"
 
-
   lazy val tapirDependencies = Seq(
     "com.softwaremill.sttp.tapir" %% "tapir-openapi-docs" % tapirVersion,
     "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
@@ -78,8 +77,8 @@ object Dependencies {
 
   // examples
   val camundaDependencies = Seq(
-    "org.springframework.boot" % "spring-boot-starter-web" % springBootVersion exclude("org.slf4j", "slf4j-api"),
-    "org.springframework.boot" % "spring-boot-starter-jdbc" % springBootVersion exclude("org.slf4j", "slf4j-api"),
+    "org.springframework.boot" % "spring-boot-starter-web" % springBootVersion exclude ("org.slf4j", "slf4j-api"),
+    "org.springframework.boot" % "spring-boot-starter-jdbc" % springBootVersion exclude ("org.slf4j", "slf4j-api"),
     "io.netty" % "netty-all" % "4.1.73.Final", // needed for Spring Boot Version > 2.5.*
     "org.camunda.bpm.springboot" % "camunda-bpm-spring-boot-starter-rest" % camundaVersion,
     "org.camunda.bpm.springboot" % "camunda-bpm-spring-boot-starter-webapp" % camundaVersion,
@@ -89,5 +88,5 @@ object Dependencies {
     // groovy support
     "org.codehaus.groovy" % "groovy-jsr223" % groovyVersion,
     "com.h2database" % "h2" % h2Version
-  ) //.map(_.exclude("org.slf4j", "slf4j-api"))
-}
+  ) // .map(_.exclude("org.slf4j", "slf4j-api"))
+end Dependencies
