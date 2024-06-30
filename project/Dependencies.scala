dc155eacb9c7bf6df4f75e36b2b3a6d9ff5a7835
Scala Steward <scala_steward@virtuslab.com>
2024-06-30 15:38:52 +0000

    Update groovy-jsr223 to 3.0.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 import sbt.Keys.*
 
-object Dependencies {
+object Dependencies:
 
   // dependency Versions
   // 00-documentation
@@ -54,7 +54,7 @@ object Dependencies {
   // examples
   val h2Version = "2.2.224"
   val twitter4jVersion = "4.1.2"
-  val groovyVersion = "3.0.21"
+  val groovyVersion = "3.0.22"
 
   lazy val tapirDependencies = Seq(
     "com.softwaremill.sttp.tapir" %% "tapir-openapi-docs" % tapirVersion,
@@ -77,8 +77,8 @@ object Dependencies {
 
   // examples
   val camundaDependencies = Seq(
-    "org.springframework.boot" % "spring-boot-starter-web" % springBootVersion exclude("org.slf4j", "slf4j-api"),
-    "org.springframework.boot" % "spring-boot-starter-jdbc" % springBootVersion exclude("org.slf4j", "slf4j-api"),
+    "org.springframework.boot" % "spring-boot-starter-web" % springBootVersion exclude ("org.slf4j", "slf4j-api"),
+    "org.springframework.boot" % "spring-boot-starter-jdbc" % springBootVersion exclude ("org.slf4j", "slf4j-api"),
     "io.netty" % "netty-all" % "4.1.110.Final", // needed for Spring Boot Version > 2.5.*
     "org.camunda.bpm.springboot" % "camunda-bpm-spring-boot-starter-rest" % camundaVersion,
     "org.camunda.bpm.springboot" % "camunda-bpm-spring-boot-starter-webapp" % camundaVersion,
@@ -89,4 +89,4 @@ object Dependencies {
     "org.codehaus.groovy" % "groovy-jsr223" % groovyVersion,
     "com.h2database" % "h2" % h2Version
   ) // .map(_.exclude("org.slf4j", "slf4j-api"))
-}
+end Dependencies
