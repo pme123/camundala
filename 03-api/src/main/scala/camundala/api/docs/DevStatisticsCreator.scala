13cf63f64dcaded909cb6851174dae49570c1ba8
Pascal Mengelt <pascal.mengelt@gmail.com>
2024-09-04 08:55:13 +0200

    Added Worker to DevStatistics.

--- 03-api/src/main/scala/camundala/api/docs/DevStatisticsCreator.scala
+++ 03-api/src/main/scala/camundala/api/docs/DevStatisticsCreator.scala
@@ -13,9 +13,28 @@ case class DevStatisticsCreator(gitBasePath: os.Path, basePath: os.Path):
          |Goes throw all projects and counts files and lines of code.
          |
          |${
+          Seq("InitWorkerDsl", "CustomWorkerDsl", "ServiceWorkerDsl")
+            .map: filter =>
+              lineCount("scala", Some(filter))
+            .map { case ftc @ FileTypeCount(fileType, projectCounts, filter) =>
+              s"""
+                 |${"*" * 20}
+                 |
+                 |File- and Line count for **${filter.get}** files:
+                 |
+                 |${projectCounts.map {
+                  case ProjectCounts(project, fileCount, lineCount) =>
+                    s" - $project: $lineCount of $fileCount Files"
+                }.mkString("\n")}
+                 |
+                 |**Total** $fileType: **${ftc.lineCount}** of **${ftc.fileCount}** Files
+                 |""".stripMargin
+            }.mkString
+        }
+         |${
           Seq("bpmn", "dmn", "groovy", "scala")
-            .map(lineCount)
-            .map { case ftc @ FileTypeCount(fileType, projectCounts) =>
+            .map(lineCount(_))
+            .map { case ftc @ FileTypeCount(fileType, projectCounts, _) =>
               s"""
                  |${"*" * 20}
                  |
@@ -29,25 +48,30 @@ case class DevStatisticsCreator(gitBasePath: os.Path, basePath: os.Path):
                  |**Total** $fileType: **${ftc.lineCount}** of **${ftc.fileCount}** Files
                  |""".stripMargin
             }.mkString
-        }""".stripMargin
+        }
+         |
+        """.stripMargin
     val path = basePath / "src" / "docs" / "devStatistics.md"
     os.write.over(path, statistics)
   end create
 
-  private def lineCount(fileType: String): FileTypeCount =
+  private def lineCount(fileType: String, filter: Option[String] = None): FileTypeCount =
     val projectCounts =
       os.list(gitBasePath)
         .filter(_.toIO.isDirectory)
         .filter(!_.toIO.isHidden)
         .map(_.baseName)
-        .map(lineCountProject(fileType, _))
-    FileTypeCount(fileType, projectCounts)
+        .map(lineCountProject(fileType, _, filter))
+    FileTypeCount(fileType, projectCounts, filter)
   end lineCount
 
-  private def lineCountProject(fileType: String, project: String): ProjectCounts =
+  private def lineCountProject(fileType: String, project: String, filter: Option[String]): ProjectCounts =
     val files = os.walk(gitBasePath / project)
       .filter(_.ext == fileType)
       .filter(_.toIO.isFile)
+      .filter: f =>
+        filter.forall(String(os.read.bytes(f)).contains)
+
     val lineCounts = files
       .map(os.read.lines)
       .map(_.size)
@@ -56,7 +80,7 @@ case class DevStatisticsCreator(gitBasePath: os.Path, basePath: os.Path):
 
   end lineCountProject
 
-  case class FileTypeCount(fileType: String, projectCounts: Seq[ProjectCounts]):
+  case class FileTypeCount(fileType: String, projectCounts: Seq[ProjectCounts], filter: Option[String]):
     lazy val lineCount =
       projectCounts.map(_.lineCount).sum
     lazy val fileCount =
